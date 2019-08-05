d00a1d545e608a7d619af959b51cfe90a1546177
justus <dj.pjn16@gmail.com>
2019-08-05 14:38:30 +0300

    Added the test_add_post_invalid test to the test_views file and removed the try function inthe test_forms

--- entries/tests/test_views.py
+++ entries/tests/test_views.py
@@ -19,10 +19,17 @@ class TestAdd(TestCase):
     assert form.is_valid() is True
     assert response.status_code == 200
 
-    def test_add_post(self):
+    def test_add_post_valid(self):
         url = reverse('add')
         response = self.client.post(url, data={'text': 'this boy'})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(
             response, '/', status_code=302,
             target_status_code=200, fetch_redirect_response=True)
+
+    def test_add_post_invalid(self):
+        form = EntryForm(data={'text': ''})
+        url = reverse('add')
+        response = self.client.post(url, data={'text': ''})
+        self.assertEqual(response.status_code, 200)
+        assert form.is_valid() is False
